7e5a607135865f03cf241a07189d10bfd60756d3
Shyamal Suhana Chandra <sapanamicrosoftware@gmail.com>
2024-04-05 11:29:29 -0500

    Modified it to work with say instead of tts

--- pdf2textmovie.sh
+++ pdf2textmovie.sh
@@ -8,12 +8,12 @@ function convert_PDF_into_text_movie {
 	local counterlocal="$1"
 	local elementlocal="$2"
 	convert -size 1920x1080 -background white -fill black -gravity center caption:"$elementlocal" frame`echo $counterlocal`.png
-	tts --text "$elementlocal" --model_name "tts_models/en/ljspeech/glow-tts" --out_path frame$counterlocal.wav
-	ffmpeg -loglevel quiet -y -i frame$counterlocal.wav frame$counterlocal.m4a
+	say -v "Zoe (Premium)" -o frame$counterlocal.aiff "$elementlocal"
+	ffmpeg -loglevel quiet -y -threads 4 -i frame$counterlocal.aiff -c:a libopus -b:a 96K frame$counterlocal.opus
 	local seconds=0
-	local seconds=`ffprobe -i frame$counterlocal.m4a -show_entries format=duration -v quiet -of csv="p=0"`
+	local seconds=`ffprobe -i frame$counterlocal.opus -show_entries format=duration -v quiet -of csv="p=0"`
 	ffmpeg -loglevel quiet -y -loop 1 -i frame$counterlocal.png -c:v libx264 -t `echo $seconds | perl -nl -MPOSIX -e 'print ceil($_);'` -pix_fmt yuv420p -vf scale=1920:1080 frame$counterlocal.mp4
-	ffmpeg -loglevel quiet -y -i frame$counterlocal.mp4 -i frame$counterlocal.m4a -t `echo $seconds | perl -nl -MPOSIX -e 'print ceil($_);'` frame_$counterlocal.mp4
+	ffmpeg -loglevel quiet -y -i frame$counterlocal.mp4 -i frame$counterlocal.opus -t `echo $seconds | perl -nl -MPOSIX -e 'print ceil($_);'` frame_$counterlocal.mp4
 	ls -lstr frame_$counterlocal.mp4	
 }
 
@@ -22,10 +22,9 @@ rm files.txt
 rm frame_*.mp4
 rm frame_lessig.mp4
 rm frame*.mp4
-rm frame*.m4a
-rm frame*.wav
-rm frame*.png
+rm frame*.opus
 rm frame*.aiff
+rm frame*.png
 
 touch files.txt
 export FILE=`ls -Art *.pdf | tail -n 1`
